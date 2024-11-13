2c5e7004cb6c79e8254cf34c7709239eb2725dee
wanjohi-lab <brian@aegirconsult.systems>
2024-11-13 22:31:24 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Nuxt Dojo',
+      meta: [
+        {name: 'description', content: 'Everything about Nuxt 3'}
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+      ]
+    }
+  },
+  runtimeConfig: {
+    currencyKey: process.env.CURRENCY_API_KEY
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit velit ex perferendis animi dolores repudiandae at, quisquam quas, reiciendis architecto, blanditiis corrupti iusto qui laboriosam molestias maiores. Enim, ipsa laborum?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p{
+        margin: 20px 0;
+    }
+</style>
